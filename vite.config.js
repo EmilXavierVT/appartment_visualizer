feec5645c207b95024a15a3e1e8f590c6a7eae65
EmilXavierVT <emilxavierthorsen@gmail.com>
2026-06-16 08:31:52 +0200

    mobile v.0.1

--- vite.config.js
+++ vite.config.js
@@ -905,7 +905,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5175,
-    allowedHosts: ['hinkesten.project-ice.dk'],
+    allowedHosts: true,
   },
   plugins: [
     {
